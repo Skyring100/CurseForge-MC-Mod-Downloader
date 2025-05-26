8d76aeeb01ba2e3d70e8d8e5e012b507da8a6c9c
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2025-05-25 21:58:52 -0700

    Now skips downloading a mod if it has been downloaded previously. Removed unnecessary sleep statements

--- ModInstaller.py
+++ ModInstaller.py
@@ -5,7 +5,16 @@ from selenium.webdriver.common.by import By
 import os
 import time
 
+# Keep track of all mod pages seen to avoid downloading the same mod many times
+mods_seen = []
+
 def download_mod(mod_page, mod_loader, version):
+    # Check if we have downloaded this mod before
+    if mod_page in mods_seen:
+        print(f"Mod '{mod_page}' has already been downloaded, skipping")
+        return
+    else:
+        mods_seen.append(mod_page)
     # Get mod loader id for search
     if mod_loader == "Forge":
         mod_loader_id = 1
@@ -18,7 +27,6 @@ def download_mod(mod_page, mod_loader, version):
     if driver.find_elements(By.XPATH, "//div[@class='error-container']"):
         print(f"Mod page '{mod_page}' does not exist, aborting")
         return
-    time.sleep(0.5)
     # Get the download link for the most recent file
     download_link = driver.find_element(By.XPATH, "//div[@class='files-table']/div[@class='file-row']/div/div/ul/li[2]/a").get_attribute("href")
     driver.get(download_link)
@@ -26,12 +34,9 @@ def download_mod(mod_page, mod_loader, version):
     time.sleep(5.5)
     # Check for any dependencies to download for the mod to function
     driver.get(mod_page+f"/relations/dependencies?page=1&type=RequiredDependency")
-    # Wait for possible dependenices to load
-    time.sleep(2)
     all_dependencies =  [ele.get_attribute("href") for ele in driver.find_elements(By.XPATH, "//div[@class='results-container']/a")]
-    time.sleep(0.5)
     for dependent in all_dependencies:
-        print(f"Dependency for {mod_page}, {dependent}")
+        print(f"Dependency for {mod_page} ==> {dependent}")
         download_mod(dependent, mod_loader, version)
 
 with open('config.toml', 'r') as f:
