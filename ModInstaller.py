385868ecd55973ade9270d928ea2f30bf3e402d9
Skyring100 <106502383+Skyring100@users.noreply.github.com>
2025-05-25 17:00:30 -0700

    Added error handling if a mod page does not exist

--- ModInstaller.py
+++ ModInstaller.py
@@ -12,22 +12,26 @@ def download_mod(mod_page, mod_loader, version):
     elif mod_loader == "Fabric":
         mod_loader_id = 4
     link = mod_page+f"/files/all?page=1&pageSize=20&version={version}&gameVersionTypeId={mod_loader_id}"
+    # Go to the the mod page with all the dowloadable files
     driver.get(link)
+    # Check if there is a 404 error for the modpage
+    if driver.find_elements(By.XPATH, "//div[@class='error-container']"):
+        print(f"Mod page '{mod_page}' does not exist, aborting")
+        return
     time.sleep(3)
-    # Download the file
+    # Get the download link for the most recent file
     download_link = driver.find_element(By.XPATH, "//div[@class='files-table']/div[@class='file-row']/div/div/ul/li[2]/a").get_attribute("href")
-    print(download_link)
     driver.get(download_link)
     time.sleep(6)
     # Check for any dependencies to download for the mod to function
     driver.get(mod_page+f"/relations/dependencies?page=1&type=RequiredDependency")
     all_dependencies = driver.find_elements(By.XPATH, "//div[@class='results-container']/a")
+    if len(all_dependencies) != 0:
+        print(f"Dependencies for {mod_page} found")
     time.sleep(3)
     for dependent in all_dependencies:
         print(f"Dependency for {mod_page}, {dependent}")
         download_mod(dependent.get_attribute("href"), mod_loader, version)
-
-
 
 with open('config.toml', 'r') as f:
     config = toml.load(f)
